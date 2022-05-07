95ff95dfccbe97ed0165280fe96f503a65882cfa
vitorfrois <vitor0frois@gmail.com>
2022-05-07 00:07:42 -0300

    backtracing 1 - laa1

--- terceiroSemestre/laa1/bkt1/main.cpp
+++ terceiroSemestre/laa1/bkt1/main.cpp
@@ -1,18 +1,42 @@
-void printPowerSet(char* set, int set_size){
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int printPowerSet(int* set, int set_size){
     
     // Set_size of power set of a set with set_size
     // n is (2^n-1)
-    unsigned int pow_set_size = pow(2, set_size);
-    int counter, j;
- 
-    // Run from counter 000..0 to 111..1
-    for (counter = 0; counter < pow_set_size; counter++) {
-        for (j = 0; j < set_size; j++) {
-            // Check if jth bit in the counter is set
+    int nSubsets = pow(2, set_size);
+    int result = 0, partialResult = 0;
+    // Run from i 000..0 to 111..1
+    for (int i = 0; i < nSubsets; i++) {
+        for (int j = 0; j < set_size; j++) {
+            // Check if jth bit in the i is set
             // If set then print jth element from set
-            if (counter & (1 << j))
-                cout << set[j];
+            if (i & (1 << j)){
+                // cout << set[j] << ",";
+                if(partialResult != 0)
+                    partialResult = partialResult xor set[j];
+                else
+                    partialResult = set[j];
+            }
         }
-        cout << endl;
+        result += partialResult;
+        partialResult = 0;
     }
+    return result;
 }
+
+int main(){
+    int setSize;
+    cin >> setSize;
+    int set[setSize];
+    for(int i = 0; i < setSize; i++)
+        cin >> set[i];
+    
+    cout << printPowerSet(set, setSize);
+    
+    return 0;
+}
+
