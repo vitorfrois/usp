cfa878a99e103c42f7bef036715f841764fec708
Vítor Fróis <vitor0frois@gmail.com>
2024-10-03 11:42:36 -0300

    finished coding

--- 8thPeriod/HighPerformanceComputing/Prova1/main.c
+++ 8thPeriod/HighPerformanceComputing/Prova1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <math.h>
 
 #define N 2
 #define T N
@@ -8,13 +9,16 @@
 int main(){
 	char linebreak;
 	int A[N][N];
+	omp_set_num_threads(T);
+
+	// Leitura da matriz
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < N; j++){
 			scanf("%d", &A[i][j]);
 		}
 	}
 
-	omp_set_num_threads(T);
+	// Soma das colunas
 	int soma_colunas[N], i, j;
 	#pragma omp parallel shared(soma_colunas) private(i, j)
 	{
@@ -23,23 +27,44 @@ int main(){
 		for(i = 0; i < N; i++)
 			soma_colunas[j] += A[i][j];
 	}
+	#pragma omp barrier
 
-	printf("%d", soma_colunas[0]);
-	printf("%d", soma_colunas[1]);
-	
+	// Média da matriz
 	double media = 0;
-	#pragma omp parallel shared(soma_colunas)
+	#pragma omp parallel for reduction(+:media) private(i) num_threads(T)
+	for(i = 0; i < N; i++)
+		media += soma_colunas[i];
+	
+	media = media / (N*N);
+	
+	// Média dos valores da matriz
+	int medial[N], soma_diferenca[N];
+	double dsvpdr[N];
+	#pragma omp parallel
 	{
-		#pragma omp for reduction(+:media)
-		for(i = 0; i < N; i++){
-			printf("media: %d", media);
-			media += soma_colunas[i];
+		#pragma omp single
+		{
+			for(j = 0; j < N; j++){
+				#pragma omp task 
+				{
+					medial[j] = 0;
+					for(i = 0; i < N; i++)
+						if (A[i][j] < media) 
+							medial[j] += 1;
+				}
+				#pragma omp task
+				{
+					soma_diferenca[j] = 0;
+					for(i = 0; i < N; i++)
+						soma_diferenca[j] += pow(A[i][j] - soma_colunas[j], 2);
+					dsvpdr[j] = sqrt(soma_diferenca[j] / N);
+				}
+			}
 		}
 	}
 
-	media = media / N;
-
-	printf("%d", media);
-
+	for(i = 0; i < N; i++)
+		printf("(%d) dsvpdr = %f; medial = %d\n", i, dsvpdr[i], medial[i]);
+	
 	return 0;
 }
